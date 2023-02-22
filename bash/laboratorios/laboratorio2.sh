e18039aa022a71d4e9d7110521a9b25d5443f13e
Jairo Buitrago <jairobuitrago3@gmail.com>
2023-02-22 00:59:13 +0000

    laboratorio 2 terminado

---
+++ bash/laboratorios/laboratorio2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Bienvenido a tú carrito de compras, ¿Deseas agregar algo al carrito?"
+eleccion=1
+
+carrito=()
+
+until [ $eleccion == 2 ]
+do
+  echo "Escoge una de las siguientes opciones:"
+  echo "1. Agregar al carrito"
+  echo "2. Finalizar compra"
+
+  read eleccion
+  if [[ $eleccion == 1 ]]; then
+    echo "Escribe lo que quieres agregar al carrito:"
+    read articulo
+    carrito+=($articulo)
+  fi
+done
+
+touch compras
+echo "${carrito[@]}" >> compras
+  
